1fbe51cba6f2a4df6e9e8e459a0f0c4fe80968d1
azuda <aaronzhang587@gmail.com>
2022-12-02 11:01:02 -0800

    Standardize format

--- 1_calorie_counting.py
+++ 1_calorie_counting.py
@@ -5,26 +5,42 @@ Calorie Counting
 """
 
 
-# Part 1
-with open("1_input.txt") as f:
-  lines = f.read().splitlines()
+def p1():
+  with open("1_input.txt") as f:
+    lines = f.read().splitlines()
 
-sums = [0]
-counter = 0
-for line in lines:
-  if line == "":
-    sums.append(0)
-    counter += 1
-  else:
-    sums[counter] += int(line)
+  sums = [0]
+  counter = 0
+  for line in lines:
+    if line == "":
+      sums.append(0)
+      counter += 1
+    else:
+      sums[counter] += int(line)
 
-print(max(sums))
+  print(max(sums))
 
 
-# Part 2
-top_three = 0
-for i in range(3):
-  top_three += max(sums)
-  sums.remove(max(sums))
+def p2():
+  with open("1_input.txt") as f:
+    lines = f.read().splitlines()
 
-print(top_three)
+  sums = [0]
+  counter = 0
+  for line in lines:
+    if line == "":
+      sums.append(0)
+      counter += 1
+    else:
+      sums[counter] += int(line)
+
+  top_three = 0
+  for i in range(3):
+    top_three += max(sums)
+    sums.remove(max(sums))
+
+  print(top_three)
+
+
+p1()
+p2()
--- 2_rock_paper_scissors.py
+++ 2_rock_paper_scissors.py
@@ -5,59 +5,66 @@ Rock Paper Scissors
 """
 
 
-# Part 1
-with open("2_input.txt") as f:
-  lines = f.read().splitlines()
+def p1():
+  with open("2_input.txt") as f:
+    lines = f.read().splitlines()
 
-score = 0
-for line in lines:
-  choose = line.split(" ")
-  if choose[1] == "X":
-    score += 1
-    if choose[0] == "A":
-      score += 3
-    elif choose[0] == "C":
-      score += 6
-  elif choose[1] == "Y":
-    score += 2
-    if choose[0] == "A":
-      score += 6
-    elif choose[0] == "B":
-      score += 3
-  elif choose[1] == "Z":
-    score += 3
-    if choose[0] == "B":
-      score += 6
-    elif choose[0] == "C":
-      score += 3
-
-print(score)
-
-
-# Part 2
-score = 0
-for line in lines:
-  choose = line.split(" ")
-  if choose[1] == "X":
-    if choose[0] == "A":
-      score += 3
-    elif choose[0] == "B":
+  score = 0
+  for line in lines:
+    choose = line.split(" ")
+    if choose[1] == "X":
       score += 1
-    elif choose[0] == "C":
+      if choose[0] == "A":
+        score += 3
+      elif choose[0] == "C":
+        score += 6
+    elif choose[1] == "Y":
       score += 2
-  elif choose[1] == "Y":
-    if choose[0] == "A":
-      score += 4
-    elif choose[0] == "B":
-      score += 5
-    elif choose[0] == "C":
-      score += 6
-  elif choose[1] == "Z":
-    if choose[0] == "A":
-      score += 8
-    elif choose[0] == "B":
-      score += 9
-    elif choose[0] == "C":
-      score += 7
+      if choose[0] == "A":
+        score += 6
+      elif choose[0] == "B":
+        score += 3
+    elif choose[1] == "Z":
+      score += 3
+      if choose[0] == "B":
+        score += 6
+      elif choose[0] == "C":
+        score += 3
 
-print(score)
+  print(score)
+
+
+def p2():
+  with open("2_input.txt") as f:
+    lines = f.read().splitlines()
+
+  score = 0
+  for line in lines:
+    choose = line.split(" ")
+    if choose[1] == "X":
+      if choose[0] == "A":
+        score += 3
+      elif choose[0] == "B":
+        score += 1
+      elif choose[0] == "C":
+        score += 2
+    elif choose[1] == "Y":
+      if choose[0] == "A":
+        score += 4
+      elif choose[0] == "B":
+        score += 5
+      elif choose[0] == "C":
+        score += 6
+    elif choose[1] == "Z":
+      if choose[0] == "A":
+        score += 8
+      elif choose[0] == "B":
+        score += 9
+      elif choose[0] == "C":
+        score += 7
+
+  print(score)
+
+
+p1()
+p2()
